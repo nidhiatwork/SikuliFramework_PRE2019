d8d9c863c72ee080e75775b533c517c0848f4f8d
Nidhi Bhushan <nidhi.bhushan123@gmail.com>
2018-09-03 17:19:45 +0530

    for mac Signed-off-by: Nidhi Bhushan <nidhi.bhushan123@gmail.com>

---
+++ BatFiles/Mac_Kill_PRE.sh
@@ -0,0 +1,2 @@
+killall Adobe Premiere Elements 2019
+killall Elements Auto Creator 17
--- TestScripts/Constants.py
+++ TestScripts/Constants.py
@@ -1,11 +1,23 @@
-import os, sys
+import os, sys, platform
 
-AppPath_PRE = "C:\Program Files\Adobe\Adobe Premiere Elements 2019\PremiereElementsEditor.exe"
-userdir = os.path.expanduser('~')
-userdir.replace("\\", "\\\\")
 
-RootFolder = userdir + "\\Desktop\\SikuliFramework_PRE2019"
-BaselineFolder = RootFolder + "\\BaselineImages\\"
-OutputFolder = RootFolder + "\\Output\\"
-TestDataFile_path = RootFolder + "\\TestData\\test.mp4"
-Sikuli_Path = userdir + "\\Downloads"
+if platform.system()=='Windows':
+    AppPath_PRE = "C:\Program Files\Adobe\Adobe Premiere Elements 2019\PremiereElementsEditor.exe"
+    userdir = os.path.expanduser('~')
+    userdir.replace("\\", "\\\\")
+
+    RootFolder = userdir + "\\Desktop\\SikuliFramework_PRE2019"
+    BaselineFolder = RootFolder + "\\BaselineImages\\"
+    OutputFolder = RootFolder + "\\Output\\"
+    TestDataFile_path = RootFolder + "\\TestData\\test.mp4"
+    Sikuli_Path = userdir + "\\Downloads"
+
+elif platform.system()=='Darwin':
+    AppPath_PRE = "/Applications/Adobe Premiere Elements 2019/Support Files/Adobe Premiere Elements.app"
+    userdir = os.path.expanduser('~')
+
+    RootFolder = userdir + "/Desktop/SikuliFramework_PRE2019"
+    BaselineFolder = RootFolder + "/BaselineImages/"
+    OutputFolder = RootFolder + "/Output/"
+    TestDataFile_path = RootFolder + "/TestData/test.mp4"
+    Sikuli_Path = userdir + "/Downloads"
