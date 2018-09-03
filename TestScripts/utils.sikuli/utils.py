32717011c56389d9d61811d0158119ce986b908b
Nidhi Bhushan <nbhushan@adobe.com>
2018-09-03 12:02:44 +0530

    raise error on failure Signed-off-by: Nidhi Bhushan <nbhushan@adobe.com>

--- TestScripts/utils.sikuli/utils.py
+++ TestScripts/utils.sikuli/utils.py
@@ -23,12 +23,14 @@ def cleanCache_And_LaunchPRE():
         type(Key.ENTER) 
         
         setAutoWaitTimeout(60)
+
         try:
                 find(getBaselineImg('Button_GoalScreen_CloseGoalScreen.png'))
         except:
                 print("Unable to launch PRE application after waiting for 60 seconds. End of execution.")
                 closePRE()
                 sys.exit(0)
+
         setAutoWaitTimeout(15)
        
       
@@ -48,7 +50,7 @@ def findElement( element ):
        except:
                 stack = traceback.extract_stack(limit = 2)
                 print "Unable to find element: " + getImageNameFromFullPath(element) + "\nBelow are details:\n" + str(sys.exc_info()[0]) + " -- line no. " + str(stack[0][1])
-                #raise
+                raise
 
 def clickElement( element ):
         try:
@@ -56,21 +58,18 @@ def clickElement( element ):
         except:
                 stack = traceback.extract_stack(limit = 2)
                 print "Unable to click element: " + getImageNameFromFullPath(element) + "\nBelow are details:\n" + str(sys.exc_info()[0]) + " -- line no. " + str(stack[0][1])
-                #raise
+                raise
 
 def assertElementExists( element ):
         try:
                 assert(exists(element))
-        except:
+        except AssertionError:
                 stack = traceback.extract_stack(limit = 2)
                 print "Unable to assert image exists: " + getImageNameFromFullPath(element) + "\nBelow are details:\n" + str(sys.exc_info()[0]) + " -- line no. " + str(stack[0][1])
-                #raise
+                raise
 
 def getImageNameFromFullPath( image_path ):
         element_path_list = image_path.split("\\")
         image_item = element_path_list[len(element_path_list)-1]
         return image_item
         
-
-
-
